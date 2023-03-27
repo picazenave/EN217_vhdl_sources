3a8acf9ab540c7a4e0b01fe2ae2c9fa7c418f5a1
Pierre Cazenave <47040310+picazenave@users.noreply.github.com>
2023-03-27 22:14:50 +0200

    good line gen need fix on image

--- matlab/untitled.m
+++ matlab/untitled.m
@@ -17,14 +17,19 @@ imshow(uint8(img_1d*255))
 
 img_edge=edge(img_1d);
 imshow(uint8(img_edge*255))
-
-
+img_edge=rot90(img_edge);
+% img_edge=rot90(img_edge);
+% img_edge=rot90(img_edge);
 %% find each line
 figure
 [h,w]=size(img_edge);
 found=false;
 counter_line=1;
 line_vector=zeros(1000,1);
+x=int32(0);
+y=int32(0);
+x2=int32(0);
+y2=int32(0);
 for i=1:w %each y
     for j=1:h %each x
 
@@ -33,12 +38,12 @@ for i=1:w %each y
                 x=j;
                 y=i;
                 found=true;
-            elseif(j~=x+1)
+            elseif(j>x+10)
                 x2=j;
                 y2=i;
-                fprintf("found x:%d y:%d x2:%d y2:%d\n",x,y,x2,y2)
+                fprintf("found x:%d y:%d x2:%d y2:%d counter_line:%d\n",x,y,x2,y2,counter_line)
                 found=false;
-                line_vector(counter_line)=bitshift(1,43)+bitshift(1,42)+bitshift(y2,29)+bitshift(x2,19)+bitshift(y,9)+x;
+                line_vector(counter_line)=bitshift(1,43)+bitshift(1,42)+bitshift(y2,30)+bitshift(x2,20)+bitshift(y,10)+x;
                 counter_line=counter_line+1;
                 hold on
                 plot([x x2],[y y2])
@@ -60,13 +65,16 @@ for i=1:w %each y
 end
 
 %% print all lines
-for i=1:size(line_vector)
-    if(line_vector(i)==0)
-        break;
-    end
-    fprintf("x""%012lx"",\n",line_vector(i));
-end
+% for i=1:size(line_vector)
+%     if(line_vector(i)==0)
+%         break;
+%     end
+%     fprintf("x""%012lx"",\n",line_vector(i));
+% end
 
+fileID = fopen('data.txt','w');
+fprintf(fileID,"x""%012lx"",\n",nonzeros(line_vector));
+fclose(fileID);
 %plot(line_vector)
 %%
 % offset_x=10;
